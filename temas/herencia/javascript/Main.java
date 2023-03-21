8939806cc95420ea0026535f44d7cfc72914a962
ZuleimaMunozJimenez <79993065+ZuleimaMunozJimenez@users.noreply.github.com>
2023-03-21 17:42:51 +0100

    Create Main.java

---
+++ temas/herencia/javascript/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args)
+{
+  LibroDeRomance miLibro = new LibroDeRomance("Antes de diciembre", "Joana Marcús", 496, "romance juvenil");
+  miLibro.imprimirInfos();
+}
+}
