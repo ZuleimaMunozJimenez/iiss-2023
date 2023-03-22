26513b914d6f06341c13785c5c451dc719afb521
ZuleimaMunozJimenez <79993065+ZuleimaMunozJimenez@users.noreply.github.com>
2023-03-22 20:17:54 +0100

    Update Libro.py

--- temas/encapsulacion/Python/Libro.py
+++ temas/encapsulacion/Python/Libro.py
@@ -1,8 +1,9 @@
 class Libro:
-    def __init__(self, titulo, autor, precio):
+    def __init__(self, titulo, autor, precio, valoracion):
         self.__titulo = titulo
         self.__autor = autor
         self.__precio = precio
+        self.__valoracion = valoracion
 
     def get_titulo(self):
         return self.__titulo
@@ -16,15 +17,33 @@ class Libro:
     def set_precio(self, precio):
         self.__precio = precio
 
-# Crear un objeto de libro
-libro1 = Libro("Antes de Diciembre", "Joana Marcús", 20)
+    def __modificar_valoracion(self, valoracion):
+        self.__valoracion = valoracion
+    def mostrar_valoracion(self):
+        return self.__valoracion
+    def aumentar_valoracion(self, libro):
+        libro.__modificar_valoracion(5)
 
-# Obtener el título y el autor del libro (uso de método get)
+
+
+# Crear un objeto de libro
+libro1 = Libro("Antes de Diciembre", "Joana Marcús", 20, 4)
+
+# Obtener el título del libro (uso de método get)
 print("Título del libro:", libro1.get_titulo())
-print("Autor del libro:", libro1.get_autor())
+print("Autor del libro: ", libro1.get_autor())
+# Intentar acceder directamente al atributo privado "__precio" causa un error
+#print(libro1.__precio)
 
 # Actualizar el precio del libro (uso de método set)
 libro1.set_precio(25)
 
 # Obtener el nuevo precio del libro (uso de método get)
 print("Precio del libro:", libro1.get_precio())
+
+print("Valoración:", libro1.mostrar_valoracion())
+
+#Intentar acceder directamente al método privado "__modificar_valoracion" causa un error
+#libro1.__modificar_valoracion(5)
+libro1.aumentar_valoracion(libro1)
+print("Valoración:", libro1.mostrar_valoracion())
