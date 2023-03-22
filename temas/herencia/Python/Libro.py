a6c66c8f5d0ea3e32c694ee605b6b4688f1bf2ff
ZuleimaMunozJimenez <79993065+ZuleimaMunozJimenez@users.noreply.github.com>
2023-03-22 16:53:53 +0100

    Update and rename temas/herencia/javascript/Libros.java to temas/herencia/Python/Libro.py

---
+++ temas/herencia/Python/Libro.py
@@ -0,0 +1,22 @@
+class Libro:
+    def __init__(self, titulo, autor, numPaginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.numPaginas = numPaginas
+
+    def imprimir(self):
+        print(f"{self.titulo} by {self.autor}, {self.numPaginas} numPaginas")
+
+class Novela(Libro):
+    def __init__(self, titulo, autor, numPaginas, genero):
+        super().__init__(titulo, autor, numPaginas)
+        self.genero = genero
+
+    def imprimir(self):
+        print(f"El título es: {self.titulo}\nEscrito por: {self.autor}\nCuenta con un total de {self.numPaginas} páginas\nSu género es {self.genero}\n")
+
+# Ejemplo de uso
+miNovela = Novela("Antes de Diciembre", "Joana Marcús", 493, "Romántico juvenil")
+miNovela2 = Novela("El mapa de los anhelos", "Alice Kellen", 496, "Romántica contemporánea")
+miNovela.imprimir()
+miNovela2.imprimir()
