6e34f5ddd548bd9e999317330bcfd92020c95c16
ZuleimaMunozJimenez <79993065+ZuleimaMunozJimenez@users.noreply.github.com>
2023-03-22 16:54:11 +0100

    Delete temas/herencia/javascript directory

--- temas/herencia/javascript/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args)
-{
-  LibroDeRomance miLibro = new LibroDeRomance("Antes de diciembre", "Joana Marcús", 496, "romance juvenil");
-  miLibro.imprimirInfos();
-}
-}
