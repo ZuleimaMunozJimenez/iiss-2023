c65a9c1d06af4a2f2472af1262f033b26fcb5e6e
ZuleimaMunozJimenez <79993065+ZuleimaMunozJimenez@users.noreply.github.com>
2023-03-23 12:53:23 +0100

    Update Main.scala

--- temas/encapsulacion/Scala/Main.scala
+++ temas/encapsulacion/Scala/Main.scala
@@ -2,7 +2,8 @@ object Main extends App {
 
   val libro = new Libro("Antes de diciembre", "Autor desconocido", "Planeta")
   println(libro) // Antes de diciembre, Autor desconocido, Planeta
-
+  //libro.titulo = "ADD"
+  libro.modtitulo_=(libro, "ADD")
   libro.autor = "Joana Marcús"
   libro.editorial = "Penguin House"
   println(libro) // Antes de diciembre, Joana Marcús, Penguin House
