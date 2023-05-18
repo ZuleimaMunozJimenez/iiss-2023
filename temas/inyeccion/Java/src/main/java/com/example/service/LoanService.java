093a1f3dab3bfa8bc4273290861199b3b8639995
ZuleimaMunozJimenez <79993065+ZuleimaMunozJimenez@users.noreply.github.com>
2023-05-18 12:21:22 +0200

    Update LoanService.java

--- temas/inyeccion/Java/src/main/java/com/example/service/LoanService.java
+++ temas/inyeccion/Java/src/main/java/com/example/service/LoanService.java
@@ -1,1 +1,12 @@
+package com.example.service;
 
+import com.example.model.Book;
+
+import java.util.List;
+
+public interface LoanService {
+    void loanBook(String title, String borrower);
+    void returnBook(String title);
+    List<Book> getLoanedBooks();
+    boolean isBookLoaned(String title);
+}
