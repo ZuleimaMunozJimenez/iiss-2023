fdee373a955759b7045b7e328cd0d5ffa8a49033
ZuleimaMunozJimenez <79993065+ZuleimaMunozJimenez@users.noreply.github.com>
2023-05-18 12:19:47 +0200

    Update BookService.java

--- temas/inyeccion/Java/src/main/java/com/example/service/BookService.java
+++ temas/inyeccion/Java/src/main/java/com/example/service/BookService.java
@@ -1,5 +1,12 @@
-// Interfaz para el servicio de libros
+package com.example.service;
+
+import com.example.model.Book;
+
+import java.util.List;
+
 public interface BookService {
     void addBook(String title, String author);
+    void removeBook(String title);
     List<Book> getAllBooks();
+    Book findBookByTitle(String title);
 }
