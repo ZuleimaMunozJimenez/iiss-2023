4fc6fedc44898a32de7531bf287abef088003298
ZuleimaMunozJimenez <79993065+ZuleimaMunozJimenez@users.noreply.github.com>
2023-05-18 12:20:51 +0200

    Update BookServiceImpl.java

--- temas/inyeccion/Java/src/main/java/com/example/service/BookServiceImpl.java
+++ temas/inyeccion/Java/src/main/java/com/example/service/BookServiceImpl.java
@@ -1,20 +1,43 @@
+package com.example.service;
 
+import com.example.model.Book;
 
-// Implementación del servicio de libros
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookServiceImpl implements BookService {
     private List<Book> books;
 
-    @Inject
     public BookServiceImpl() {
-        books = new ArrayList<>();
+        this.books = new ArrayList<>();
     }
 
+    @Override
     public void addBook(String title, String author) {
         Book book = new Book(title, author);
         books.add(book);
     }
 
+    @Override
+    public void removeBook(String title) {
+        Book bookToRemove = findBookByTitle(title);
+        if (bookToRemove != null) {
+            books.remove(bookToRemove);
+        }
+    }
+
+    @Override
     public List<Book> getAllBooks() {
-        return books;
+        return new ArrayList<>(books);
+    }
+
+    @Override
+    public Book findBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                return book;
+            }
+        }
+        return null;
     }
 }
