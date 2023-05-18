c96a22b6e6da389e32935f919b37cdab24b90d00
ZuleimaMunozJimenez <79993065+ZuleimaMunozJimenez@users.noreply.github.com>
2023-05-18 12:18:14 +0200

    Update LibraryApp.java

--- temas/inyeccion/Java/src/main/java/com/example/LibraryApp.java
+++ temas/inyeccion/Java/src/main/java/com/example/LibraryApp.java
@@ -1,4 +1,9 @@
-// Clase principal de la aplicación
+package com.example;
+
+import com.example.service.BookService;
+import com.example.service.LoanService;
+import com.google.inject.Inject;
+
 public class LibraryApp {
     private final BookService bookService;
     private final LoanService loanService;
@@ -12,5 +17,17 @@ public class LibraryApp {
     public void run() {
         // Lógica de la aplicación de la biblioteca
         // utilizando los servicios inyectados
+        bookService.addBook("El Gran Gatsby", "F. Scott Fitzgerald");
+        bookService.addBook("Cien años de soledad", "Gabriel García Márquez");
+        bookService.addBook("1984", "George Orwell");
+
+        loanService.loanBook("El Gran Gatsby", "John Doe");
+        loanService.loanBook("1984", "Jane Smith");
+
+        System.out.println("Libros disponibles:");
+        bookService.getAllBooks().forEach(System.out::println);
+
+        System.out.println("Libros prestados:");
+        loanService.getLoanedBooks().forEach(System.out::println);
     }
 }
