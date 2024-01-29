f96e4f12cfb9c050810798a4598b291d90639083
ZuleimaMunozJimenez <79993065+ZuleimaMunozJimenez@users.noreply.github.com>
2024-01-29 13:32:27 +0100

    Delete temas/delegacion/Scala directory

--- temas/delegacion/Scala/Compra.scala
+++
@@ -1,7 +0,0 @@
-class Compra {
-  def calcularPrecioTotal(libros: List[Libro], politicaDescuento: PoliticaDescuento): Double = {
-    val descuento = politicaDescuento.calcularDescuento(libros)
-    val precioTotal = libros.map(_.precio).sum
-    precioTotal - (precioTotal * descuento)
-  }
-}
--- temas/delegacion/Scala/Libro.scala
+++
@@ -1,1 +0,0 @@
-case class Libro(titulo: String, autor: String, precio: Double)
